1aec6f832f1b9b03f914a8438b74aaaeeda4b7e0
Dante Palumbo <106630028+DantePalu@users.noreply.github.com>
2024-08-08 09:40:28 -0300

    Add files via upload

---
+++ app comunicacion/js/app.js
@@ -0,0 +1,27 @@
+
+var sound = new Audio()
+sound.src = "./BIBLIO.mp3"
+
+var sound1 = new Audio()
+sound1.src = "./MEDICC.mp3"
+
+var sound2 = new Audio()
+sound2.src = "./bano-accesible-audio.mp3"
+
+var sound3 = new Audio()
+sound3.src = "./podotactil.mp3"
+
+var sound4 = new Audio()
+sound4.src = "./PLATAFORMACC.mp3"
+
+var sound5 = new Audio()
+sound5.src = "./AROMG.mp3"
+
+var sound6 = new Audio()
+sound6.src = "./LACTARIO.mp3"
+
+var sound7 = new Audio()
+sound7.src = "./RAMPA.mp3"
+
+var sound8 = new Audio()
+sound8.src = "./PARK.mp3"
